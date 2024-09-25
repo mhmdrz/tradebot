2833c2d96d8048a0b85d5e83d1be11c4181ea5be
MohammadReza <mhmdrz.abd@gmail.com>
2024-09-25 19:41:53 +0330

    Remove unused package Add volatility based stop loss

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,6 @@
 import MetaTrader5 as mt5
 import pandas as pd
-from strategies import zscore, trend
+from strategies import zscore, trend, volatility
 from utils import sessions, botlog, utils, crawler
 import numpy as np
 import time
@@ -20,10 +20,10 @@ class Bot:
         self.is_market_open = sessions.market_time_check()
 
         if not mt5.initialize():
-            print("Initialization failed")
+            print('Initialization failed')
             mt5.shutdown()
         
-        print("Initialized...")
+        print('Initialized...')
     
     def shutdown(self):
         mt5.shutdown()
@@ -36,6 +36,7 @@ class Bot:
     
     def calculate_signals(self):
         data = self.get_data()
+        data = volatility.calculate_atr(data, 14)
         data = trend.calculate_ema(data, 200)
         data = trend.calculate_g(data, 20)
 
@@ -45,17 +46,21 @@ class Bot:
         lot_size = (balance * self.balance_percentage * 0.01) * (self.sl_pips * 0.0001 * 10)
         return round(lot_size, 2)
     
-    def calculate_dynamic_sl(self, data, window):
-        prices = data['close']
-        sl_pips = 0.5 * np.std(prices[-window:])
-
-        return sl_pips
+    def calculate_dynamic_sl(self, latest, multiplier):
+        if type == 'buy':
+            stoploss = latest['close'] - (latest['atr'] * multiplier)
+        elif type == 'sell':
+            stoploss = latest['close'] + (latest['atr'] * multiplier)
+        else:
+            print("Error setting atr based stop loss!")
+        
+        return stoploss
     
     def manage_trailing(self, order_ticket, order_type):
         while True:
             positions = mt5.positions_get(ticket=order_ticket)
             if not positions:
-                print("No open positions found for trailing stop.")
+                print('No open positions found for trailing stop.')
                 break
 
             position = positions[0]
@@ -64,49 +69,49 @@ class Bot:
 
             if (order_type == 'buy' and new_sl_price > position.sl) or (order_type == 'sell' and new_sl_price < position.sl):
                 request = {
-                    "action": mt5.TRADE_ACTION_SLTP,
-                    "position": position.ticket,
-                    "sl": new_sl_price,
-                    "tp": position.tp,
+                    'action': mt5.TRADE_ACTION_SLTP,
+                    'position': position.ticket,
+                    'sl': new_sl_price,
+                    'tp': position.tp,
                 }
                 result = mt5.order_send(request)
                 if result.retcode != mt5.TRADE_RETCODE_DONE:
-                    print(f"Failed to update trailing stop: {result.retcode}")
-                    botlog.logger.info(f"Failed to update trailing stop: {result.retcode}")
+                    print(f'Failed to update trailing stop: {result.retcode}')
+                    botlog.logger.info(f'Failed to update trailing stop: {result.retcode}')
                 else:
-                    print(f"Trailing stop updated to: {new_sl_price}")
-                    botlog.logger.info(f"Trailing stop updated to: {new_sl_price}")
+                    print(f'Trailing stop updated to: {new_sl_price}')
+                    botlog.logger.info(f'Trailing stop updated to: {new_sl_price}')
             
             time.sleep(25)
     
     def place_order(self, order_type, lot_size, price, sl_price, tp_price):
         if order_type == 'buy':
             request = {
-                "action": mt5.TRADE_ACTION_DEAL,
-                "symbol": self.symbol,
-                "volume": lot_size,
-                "type": mt5.ORDER_TYPE_BUY,
-                "price": price,
-                "sl": sl_price,
-                "tp": tp_price,
+                'action': mt5.TRADE_ACTION_DEAL,
+                'symbol': self.symbol,
+                'volume': lot_size,
+                'type': mt5.ORDER_TYPE_BUY,
+                'price': price,
+                'sl': sl_price,
+                'tp': tp_price,
             }
         elif order_type == 'sell':
             request = {
-                "action": mt5.TRADE_ACTION_DEAL,
-                "symbol": self.symbol,
-                "volume": lot_size,
-                "type": mt5.ORDER_TYPE_SELL,
-                "price": price,
-                "sl": sl_price,
-                "tp": tp_price,
+                'action': mt5.TRADE_ACTION_DEAL,
+                'symbol': self.symbol,
+                'volume': lot_size,
+                'type': mt5.ORDER_TYPE_SELL,
+                'price': price,
+                'sl': sl_price,
+                'tp': tp_price,
             }
         result = mt5.order_send(request)
         if result.retcode != mt5.TRADE_RETCODE_DONE:
-            print(f"Order failed: {result.retcode}")
-            botlog.logger.info(f"Order failed: {result.retcode}")
+            print(f'Order failed: {result.retcode}')
+            botlog.logger.info(f'Order failed: {result.retcode}')
         else:
-            print(f"Order placed successfully: {result}")
-            botlog.logger.info(f"Order placed successfully: {result}")
+            print(f'Order placed successfully: {result}')
+            botlog.logger.info(f'Order placed successfully: {result}')
             order_ticket = result.order
             if self.trailing_pips:
                 self.manage_trailing(order_ticket, order_type)
@@ -145,8 +150,6 @@ class Bot:
                 sl_pips = self.sl_pips * 0.0001
             elif self.sl_pips and decimal <= 2:
                 sl_pips = self.sl_pips * 0.01
-            else:
-                sl_pips = self.calculate_dynamic_sl(data, 20)
 
             if self.tp_pips and decimal > 2:
                 tp_pips = self.tp_pips * 0.0001
@@ -154,18 +157,18 @@ class Bot:
                 tp_pips = self.tp_pips * 0.01
             
             if latest['bullish'] and latest['close'] < latest['ema']:
-                sl_price = ask_price - sl_pips
+                sl_price = self.calculate_dynamic_sl(latest, 1.5, 'buy') if not self.sl_pips else ask_price - sl_pips
                 tp_price = ask_price + tp_pips if self.tp_pips else ask_price + (sl_pips * self.rr_ratio)
                 self.place_order('buy', lot_size, ask_price, sl_price, tp_price)
             elif not latest['bullish'] and latest['close'] > latest['ema']:
-                sl_price = bid_price + sl_pips
+                sl_price = self.calculate_dynamic_sl(latest, 1.5, 'buy') if not self.sl_pips else ask_price + sl_pips
                 tp_price = bid_price - tp_pips if self.tp_pips else bid_price - (sl_pips * self.rr_ratio)
                 self.place_order('sell', lot_size, bid_price, sl_price, tp_price)
             
             time.sleep(180)
     
     def get_events(self):
-        return crawler.get_events('https://forexfactory.com/', headers={ "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:129.0) Gecko/20100101 Firefox/129.0" })
+        return crawler.get_events('https://forexfactory.com/', headers={ 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:129.0) Gecko/20100101 Firefox/129.0' })
     
     def main_loop(self):
         times = self.get_events()
